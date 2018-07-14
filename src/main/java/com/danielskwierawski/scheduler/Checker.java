6738865b657563eea61d0acdb9eead4969521e1f
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-14 20:02:55 +0200

    simple tests check the start time

--- src/main/java/com/danielskwierawski/scheduler/Checker.java
+++ src/main/java/com/danielskwierawski/scheduler/Checker.java
@@ -5,12 +5,22 @@ public class Checker {
     public static boolean checkStartHours(int[][] plan) {
         for (int i = 0; i < plan.length; i++) {
             for (int j = 1; j < plan[i].length; j++) {
-                if (plan[i][j] < plan[i][j - 1]) {
-                    return false;
+                if (dayIsNotDayOff(plan[i][j])) {
+                    if (dayStartsBeforeThanDayBefore(plan, i, j)) {
+                        return false;
+                    }
                 }
             }
         }
         return true;
+    }
+
+    private static boolean dayIsNotDayOff(int i) {
+        return i != 0;
+    }
+
+    private static boolean dayStartsBeforeThanDayBefore(int[][] plan, int i, int j) {
+        return plan[i][j] < plan[i][j - 1];
     }
 
 }
--- src/test/java/com/danielskwierawski/scheduler/CheckerTest.java
+++ src/test/java/com/danielskwierawski/scheduler/CheckerTest.java
@@ -7,13 +7,32 @@ import org.junit.Test;
 public class CheckerTest {
 
     @Test
-    public void shouldReturnTrueWhenEveryDayStartsAtTheSameTimeThanBeforeOrLater() throws Exception {
+    public void shouldReturnTrueWhenEveryDayStartsAtTheSameTimeThanDayBeforeOrLater() throws Exception {
         // given
         final int[][] plan = {
                 {6,  6,  8,  8,  14},
                 {14, 14, 14, 14, 14},
                 {0,  0,  6,  6,  8}};
         // when
+        boolean result = Checker.checkStartHours(plan);
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldReturnFalseIfAnyDayStartsEarlierThanDayBefore() throws Exception {
+        // given
+        final int[][] plan = {{14,  6}};
+        // when
+        boolean result = Checker.checkStartHours(plan);
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void shouldTreatZeroAsDayOff() throws Exception {
+        // given
+        final int[][] plan = {{14,  0}};
         boolean result = Checker.checkStartHours(plan);
         // then
         assertThat(result).isTrue();
