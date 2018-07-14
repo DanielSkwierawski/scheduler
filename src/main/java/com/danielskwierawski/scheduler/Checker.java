3a7608c65393c063552db434bbee5c2f36fe6adb
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-14 23:28:36 +0200

    tests for working coverage

--- src/main/java/com/danielskwierawski/scheduler/Checker.java
+++ src/main/java/com/danielskwierawski/scheduler/Checker.java
@@ -52,4 +52,30 @@ public class Checker {
         }
         return amountOfWorkingHoursOfWorker;
     }
+
+    public static boolean checkIfWorkingCoverageIsFulfilled(int[][] plan, int[][] workersCoverage) {
+        for (int i = 0; i < workersCoverage.length; i++) {
+            for (int j = 0; j < workersCoverage[i].length; j++) {
+                if (isWorkingCoverageFulfilledForGivenHour(plan, workersCoverage[i][j], i, (j + 1))) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isWorkingCoverageFulfilledForGivenHour(int[][] plan, int neededWorkersCoverage, int day, int currentHour) {
+        int amountOfAvailableWorkers = 0;
+        for (int k = 0; k < plan.length; k++) {
+            int hourOfStartWorking = plan[k][day];
+            if (dayIsNotDayOff(hourOfStartWorking)) {
+                if (isWorkerWorkingAtGivenHour(currentHour, hourOfStartWorking)) {
+                    amountOfAvailableWorkers++;
+                }
+            }
+        }
+        return amountOfAvailableWorkers < neededWorkersCoverage;
+    }
+
+    private static boolean isWorkerWorkingAtGivenHour(int currentHour, int hourOfStartWorking) {
+        return currentHour >= hourOfStartWorking && currentHour < (hourOfStartWorking + AMOUNT_OF_WORKING_HOURS_PER_DAY);
+    }
 }
--- src/test/java/com/danielskwierawski/scheduler/CheckerTest.java
+++ src/test/java/com/danielskwierawski/scheduler/CheckerTest.java
@@ -90,4 +90,39 @@ public class CheckerTest {
         // then
         assertThat(result).isFalse();
     }
+
+    @Test
+    public void shouldReturnTrueIfWorkingCoverageIsFulfilled() throws Exception {
+        // given
+        final int[][] workersCoverage = {
+//              {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+                {0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},
+                {0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},
+                {0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},
+                {0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},
+                {0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0}};
+        final int[][] plan = {
+                {6,  6,  6,  6,  6},
+                {8,  8,  8,  8,  8},
+                {14, 14, 14, 14, 14}};
+        // when
+        boolean result = Checker.checkIfWorkingCoverageIsFulfilled(plan, workersCoverage);
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldReturnFalseIfWorkingCoverageIsNotFulfilled() throws Exception {
+        // given
+        final int[][] workersCoverage = {
+//              {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+                {0,  0,  0,  0,  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  0,  0}};
+        final int[][] plan = {
+                {6,  6,  6,  6,  6},
+                {14, 14, 14, 14, 14}};
+        // when
+        boolean result = Checker.checkIfWorkingCoverageIsFulfilled(plan, workersCoverage);
+        // then
+        assertThat(result).isFalse();
+    }
 }
