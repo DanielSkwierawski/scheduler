73952111bb829f3584366029b92e59230f71b07e
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-15 20:08:20 +0200

    adjust to new version of method

--- src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
+++ src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
@@ -2,16 +2,13 @@ package com.danielskwierawski.scheduler;
 
 import java.util.Arrays;
 
-import static com.danielskwierawski.scheduler.Checker.EARLIEST_HOUR_OF_START_WORKING;
-
 public class SimpleApp {
 
     public static void main(String[] args) {
         final int amountOfWorkers = 2;
         final int amountOfDays = 2;
-        final int initValue = EARLIEST_HOUR_OF_START_WORKING;
 
-        int[][] plan = Creator.initializePlanWithGivenValue(amountOfWorkers, amountOfDays, initValue);
+        int[][] plan = Creator.initializePlanWithGivenValue(amountOfWorkers, amountOfDays);
 
         do {
             System.out.println(Arrays.deepToString(plan));
