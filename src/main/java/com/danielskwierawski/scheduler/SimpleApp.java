61ceca9d2e9b301e01ff5fba7c5c474d25242563
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-16 11:46:06 +0200

    time measurements

--- src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
+++ src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
@@ -5,14 +5,27 @@ import java.util.Arrays;
 public class SimpleApp {
 
     public static void main(String[] args) {
-        final int amountOfWorkers = 2;
-        final int amountOfDays = 2;
+
+        long timeStart;
+        long timeStop;
+        int counter = 15;
+        long[] totals = new long[counter];
+
+        final int amountOfWorkers = 1;
+        final int amountOfDays = 10;
 
         int[][] plan = Creator.initializePlanWithGivenValue(amountOfWorkers, amountOfDays);
 
-        do {
-            System.out.println(Arrays.deepToString(plan));
-        } while (Creator.increaseByOne(plan));
+        System.out.println("amountOfWorkers=" + amountOfWorkers + " amountOfDays:" + amountOfDays);
+        for (int i = 0; i < counter; i++) {
+            timeStart = System.nanoTime();
+            do {
+//            System.out.println(Arrays.deepToString(plan));
+            } while (Creator.increaseByOne(plan));
+            timeStop = System.nanoTime();
+            totals[i] = timeStop - timeStart;
+            System.out.println(totals[i]);
+        }
     }
 
 }
