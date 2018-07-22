9affcc11db5cd33843382afbe85c73025c0d190c
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-22 18:49:27 +0200

    creating every combination for 28 days with 8 non-working days

--- src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
+++ src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
@@ -8,19 +8,24 @@ public class SimpleApp {
 
         long timeStart;
         long timeStop;
-        int counter = 15;
+        int counter = 1;
         long[] totals = new long[counter];
 
         final int length = 28;
+        final int amountOfNonWorkingDays = 8;
 
         boolean[] plan = Creator.initializePlanOfWorkingDays(length);
 
         System.out.println("1DimensionalPlanOfWorkingDays length=" + length);
         for (int i = 0; i < counter; i++) {
             timeStart = System.nanoTime();
+            int amountOfCombination = 0;
             do {
-//            System.out.println(Arrays.toString(plan));
+                if (Checker.checkAmountOfNonWorkingDays(plan, amountOfNonWorkingDays)) {
+                    amountOfCombination++;
+                }
             } while (Creator.increaseByOnePlanOfWorkingDays(plan));
+            System.out.println(amountOfCombination);
             timeStop = System.nanoTime();
             totals[i] = timeStop - timeStart;
             System.out.println(totals[i]);
