bdaccac795b0114861dba4274f6699e2387ae166
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-22 18:08:59 +0200

    generating plans of working days

--- src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
+++ src/main/java/com/danielskwierawski/scheduler/SimpleApp.java
@@ -11,16 +11,16 @@ public class SimpleApp {
         int counter = 15;
         long[] totals = new long[counter];
 
-        final int length = 10;
+        final int length = 28;
 
-        int[] plan = Creator.initializePlan(length);
+        boolean[] plan = Creator.initializePlanOfWorkingDays(length);
 
-        System.out.println("1DimensionalPlan length=" + length + " HP ProBook 640 G3");
+        System.out.println("1DimensionalPlanOfWorkingDays length=" + length);
         for (int i = 0; i < counter; i++) {
             timeStart = System.nanoTime();
             do {
 //            System.out.println(Arrays.toString(plan));
-            } while (Creator.increaseByOne(plan));
+            } while (Creator.increaseByOnePlanOfWorkingDays(plan));
             timeStop = System.nanoTime();
             totals[i] = timeStop - timeStart;
             System.out.println(totals[i]);
