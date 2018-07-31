efb2a871168b2f2f4c653021c0f16a952731c275
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2018-07-31 02:12:21 +0200

    time measurement

--- src/main/java/com/danielskwierawski/scheduler/SimpleAppBasedOnWorkingDaysPlan.java
+++ src/main/java/com/danielskwierawski/scheduler/SimpleAppBasedOnWorkingDaysPlan.java
@@ -6,8 +6,13 @@ public class SimpleAppBasedOnWorkingDaysPlan {
 
     public static void main(String[] args) {
 
-        final int amountOfDays = 12;
-        final int amountOfNonWorkingDays = 2;
+        long timeStart;
+        long timeStop;
+        int counter = 15;
+        long[] totals = new long[counter];
+
+        final int amountOfDays = 13;
+        final int amountOfNonWorkingDays = 4;
         final int amountOfWorkers = 3;
         final int[][] workersCoverage = {
 //              {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
@@ -23,7 +28,7 @@ public class SimpleAppBasedOnWorkingDaysPlan {
                 {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},//10
                 {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},//11
                 {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},//12
-//                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1,  0,  0},//13
+                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1,  0,  0},//13
 //                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1,  0,  0},//14
 //                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},//15
 //                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0},//16
@@ -40,18 +45,23 @@ public class SimpleAppBasedOnWorkingDaysPlan {
 //                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1,  0,  0},//27
 //                {0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1,  0,  0}//28
         };
+        final int amountOfDaysFromWorkersCoverage = workersCoverage.length;
 
         boolean[][] everyPossibleCombinationOfWorkingAndNonWorkingDays = PlanOfWorkingCreator.createEveryPossibleCombinationsOfWorkingAndNonWorkingDays(amountOfDays, amountOfNonWorkingDays);
-
         boolean[][] plan = PlanOfWorkingCreator.initializePlanForWorkersFromGivenArray(amountOfWorkers, everyPossibleCombinationOfWorkingAndNonWorkingDays);
 
-        do {
-            if (Checker.checkIfWorkingCoverageCouldBeFulfilled(plan, workersCoverage)) {
-                System.out.println(Arrays.deepToString(plan));
-            }
+        System.out.println("amountOfDays=" + amountOfDays + " amountOfNonWorkingDays=" + amountOfNonWorkingDays + " amountOfWorkers=" + amountOfWorkers + " amountOfDaysFromWorkersCoverage=" + amountOfDaysFromWorkersCoverage);
+        for (int i = 0; i < counter; i++) {
+            timeStart = System.nanoTime();
+            do {
+                if (Checker.checkIfWorkingCoverageCouldBeFulfilled(plan, workersCoverage)) {
+//                    System.out.println(Arrays.deepToString(plan));
+                }
 
-        } while (PlanOfWorkingCreator.increaseByOne(plan, everyPossibleCombinationOfWorkingAndNonWorkingDays));
-
+            } while (PlanOfWorkingCreator.increaseByOne(plan, everyPossibleCombinationOfWorkingAndNonWorkingDays));
+            timeStop = System.nanoTime();
+            totals[i] = timeStop - timeStart;
+            System.out.println(totals[i]);
+        }
     }
-
 }
